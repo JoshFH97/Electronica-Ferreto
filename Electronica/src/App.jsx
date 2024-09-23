50b1d4293b3ace574867ee72ff2f355235e922d2
IsaacFerreto <168207077+IsaacFerreto@users.noreply.github.com>
2024-09-23 13:49:03 -0600

    Update

--- Electronica/src/App.jsx
+++ Electronica/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/login'
+import Inicio from './pages/Inicio'
 
 function App() {
   
 
   return (
     <>
-  <Login/>
+  <Inicio/>
   
     </>
   )
