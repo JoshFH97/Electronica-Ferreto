860db7e57040f30354b58690175b5c45179bbbb9
JoshFH97 <joshelxd@gmail.com>
2024-09-24 15:02:47 -0600

    Creacion del jsx y el css del home page....Joshua

--- Electronica/src/App.jsx
+++ Electronica/src/App.jsx
@@ -3,7 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/login'
-import Inicio from './pages/Inicio'
+import Inicio from './pages/Home'
+import './pages/Home.css'
+
 
 function App() {
   
