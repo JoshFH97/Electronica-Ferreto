43488816e81d708518554adaeca032d5319d657e
IsaacFerreto <168207077+IsaacFerreto@users.noreply.github.com>
2024-09-24 11:37:57 -0600

    middaywork

--- Electronica/src/App.jsx
+++ Electronica/src/App.jsx
@@ -4,13 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/login'
 import Inicio from './pages/Inicio'
+import Register from './pages/Register'
 
 function App() {
   
 
   return (
     <>
-  <Inicio/>
+  <Register/>
   
     </>
   )
