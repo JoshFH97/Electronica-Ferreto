d690d80e3ba766e6b64053e5fc1970aaa0233241
JoshFH97 <joshelxd@gmail.com>
2024-09-27 15:13:07 -0600

    Routes

--- Electronica/src/App.jsx
+++ Electronica/src/App.jsx
@@ -1,26 +1,41 @@
+import * as ReactDOM from "react-dom/client";
 import './App.css'
-import Login from './pages/Login'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Home from './pages/Home'
 import './pages/Home.css'
+import Login from './pages/Login'
 import Register from './pages/Register'
 import ContactSection from './pages/Contact'
 import AboutSection from './pages/About'
 import ProductSection from './pages/Products'
 
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+]);
 
 function App() {
   
+{/* <ContactSection/> */}
+{/* <Home/>  */}
+{/* <AboutSection/> */}
+{/* <ProductSection/> */}
+{/* <Login/> */}
+{/* <Register/> */}
+
+
+
+ 
+  
 
   return (
-    <>
-  {/* <ContactSection/> */}
-  <Home/> 
-  {/* <AboutSection/> */}
-  {/* <ProductSection/> */}
-  {/* <Login/> */}
-  {/* <Register/> */}
-    </>
+    <RouterProvider router={router} />
     
   )
 }
