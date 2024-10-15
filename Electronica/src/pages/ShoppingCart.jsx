adf5bcc2346641c3b075190fee03dab78c0c2a94
IsaacFerreto <168207077+IsaacFerreto@users.noreply.github.com>
2024-10-15 08:36:24 -0600

    changi component side

--- Electronica/src/pages/ShoppingCart.jsx
+++ Electronica/src/pages/ShoppingCart.jsx
@@ -3,6 +3,7 @@ import "../ShoppingCart.css";
 import Navbar from "../Components/Navbar";
 import usingFetch from '../hooks/usingFetch.js';
 import Cookies from 'js-cookie';
+import Payments from "../Components/Payments.jsx";
 
 const ShoppingCart = () => {
   const [reload, setReload] = useState(false);
@@ -161,43 +162,7 @@ const ShoppingCart = () => {
                         <h5 className="fw-bold mb-0">{finalTotal}$</h5>
                       </div>
                     </div>
-                    <div className="col-lg-6 px-5 py-4">
-                      <h3 className="mb-5 pt-2 text-center fw-bold text-uppercase">Payment</h3>
-                      <form className="mb-5">
-                        <div className="form-outline mb-5">
-                          <input type="text" id="typeText" className="form-control form-control-lg" value=" " readOnly />
-                          <label className="form-label" htmlFor="typeText">Card Number</label>
-                        </div>
-                        <div className="form-outline mb-5">
-                          <input type="text" id="typeName" className="form-control form-control-lg" value=" " readOnly />
-                          <label className="form-label" htmlFor="typeName">Name on card</label>
-                        </div>
-                        <div className="row">
-                          <div className="col-md-6 mb-5">
-                            <div className="form-outline">
-                              <input type="text" id="typeExp" className="form-control form-control-lg" value=" " readOnly />
-                              <label className="form-label" htmlFor="typeExp">Expiration</label>
-                            </div>
-                          </div>
-                          <div className="col-md-6 mb-5">
-                            <div className="form-outline">
-                              <input type="password" id="typeCvv" className="form-control form-control-lg" value=" " readOnly />
-                              <label className="form-label" htmlFor="typeCvv">Cvv</label>
-                            </div>
-                          </div>
-                        </div>
-                        <p className="mb-5">
-                          Lorem ipsum dolor sit amet consectetur, adipisicing elit{" "}
-                          <a href="#!">obcaecati sapiente</a>.
-                        </p>
-                        <button type="button" className="btn btn-primary btn-block btn-lg">Buy now</button>
-                        <h5 className="fw-bold mb-5" style={{ position: "absolute", bottom: 0 }}>
-                          <a href="#!">
-                            <i className="fas fa-angle-left me-2" />Back to shopping
-                          </a>
-                        </h5>
-                      </form>
-                    </div>
+                    <Payments/>
                   </div>
                 </div>
               </div>
@@ -207,6 +172,7 @@ const ShoppingCart = () => {
       </section>
     </>
   );
+  
 };
 
 export default ShoppingCart;
